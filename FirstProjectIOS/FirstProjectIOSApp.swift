f381c59df8ed3883c0f6d80cc61824764acbb9ce
RodrigoMas <rodevmasg@gmail.com>
2025-05-14 16:48:12 -0600

    Initial Commit

---
+++ FirstProjectIOS/FirstProjectIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectIOSApp.swift
+//  FirstProjectIOS
+//
+//  Created by Rodrigo Martin on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstProjectIOSTests/FirstProjectIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectIOSTests.swift
+//  FirstProjectIOSTests
+//
+//  Created by Rodrigo Martin on 14/05/25.
+//
+
+import Testing
+@testable import FirstProjectIOS
+
+struct FirstProjectIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
