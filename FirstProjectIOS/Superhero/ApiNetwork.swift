57b9c0226c5bf7d62712ab91e248a03f00cf8afd
srodevs <srodevs@gmail.com>
2025-07-04 23:47:27 -0600

    SuperHero

---
+++ FirstProjectIOS/Superhero/ApiNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  ApiNetwork.swift
+//  FirstProjectIOS
+//
+//  Created by Rodrigo Martin on 04/07/25.
+//
+
+import Foundation
---
+++ FirstProjectIOS/Superhero/SuperHeroDetailUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SuperHeroDetailUIView.swift
+//  FirstProjectIOS
+//
+//  Created by Rodrigo Martin on 04/07/25.
+//
+
+import SwiftUI
+
+struct SuperHeroDetailUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperHeroDetailUIView()
+}
